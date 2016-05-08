a6540cdefe93722851b2fb2366765e883f9db7ce
Nickesh <Nickesh@users.noreply.github.com>
2016-05-08 10:53:35 +0200

    Zalążek listy kierowców...

--- assets/scripts/renderer.js
+++ assets/scripts/renderer.js
@@ -9,19 +9,28 @@ var Renderer = function(options) {
 }
 
 Renderer.prototype.addDriver = function(driver) {
-	driver.container = this.config.driverTemplate.clone();
-	this.updateDriver(driver);
+	this.draw(driver);
 	this.config.rankingContainer.append(driver.container);
 	this.drivers.push(driver);
 	return this;
 }
 
-Renderer.prototype.updateDriver = function(driver) {
-	driver.container.find('.position').text((this.drivers.length + 1) + '.');
+Renderer.prototype.draw = function(driver) {
+	if (!driver.container) {
+		driver.container = this.config.driverTemplate.clone();
+	}
 	driver.container.find('.carNumber').text(driver.number);
 	driver.container.find('.nick').text(driver.nick);
 	driver.container.find('.personalBest .time').text(driver.best).toggle(!!driver.best);
 	driver.container.find('.personalLast .time').text(driver.last).toggle(!!driver.last);
+	this.drawPositions();
+}
+
+Renderer.prototype.drawPositions = function() {
+	var i;
+	for (i in this.drivers) {
+		this.drivers[i].container.find('.position').text((i + 1) + '.');
+	}
 }
 
 var r = new Renderer();
