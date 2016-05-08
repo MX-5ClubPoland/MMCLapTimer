424b3b98ead584a02118148eb7c3cf00ac3c1f1b
Maciej Piecha <maciej.piecha@monit24.pl>
2016-05-08 10:37:33 +0200

    Test

--- assets/scripts/renderer.js
+++ assets/scripts/renderer.js
@@ -4,16 +4,20 @@ var Renderer = function(options) {
 		rankingContainer: $('.ranking'),
 		driverTemplate: $('.driverTemplate:first .driver').clone()
 	}, options);
+
+	this.drivers = [];
 }
 
 Renderer.prototype.addDriver = function(driver) {
 	driver.container = this.config.driverTemplate.clone();
 	this.updateDriver(driver);
 	this.config.rankingContainer.append(driver.container);
+	this.drivers.push(driver);
 	return this;
 }
 
 Renderer.prototype.updateDriver = function(driver) {
+	driver.container.find('.position').text((this.drivers.length + 1) + '.');
 	driver.container.find('.carNumber').text(driver.number);
 	driver.container.find('.nick').text(driver.nick);
 	driver.container.find('.personalBest .time').text(driver.best).toggle(!!driver.best);
