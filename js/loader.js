a2539cae362421d02cc0cce0dba28fc62b4f1b1a
Jakub Żurawski <jacobs242@yahoo.pl>
2016-05-10 16:22:31 +0200

    removed unnecessary clousure

--- js/loader.js
+++ js/loader.js
@@ -1,31 +1,29 @@
-$(document).ready(function () {
-	MMCLapTimer.loader = (function loader() {
-		var $loader = $('#loadingOverlay');
-		var $spinner = $('#spinner');
-		var $error = $('#loadingError');
+$(document).ready(function loaderInit() {
+	var $loader = $('#loadingOverlay');
+	var $spinner = $('#spinner');
+	var $error = $('#loadingError');
 
-		var show = function show() {
-			$error.hide();
-			$loader.removeClass('alert').show();
-			$spinner.show();
-		};
+	var show = function show() {
+		$error.hide();
+		$loader.removeClass('alert').show();
+		$spinner.show();
+	};
 
-		var hide = function hide() {
-			$error.hide();
-			$loader.removeClass('alert').hide();
-			$spinner.hide();
-		};
+	var hide = function hide() {
+		$error.hide();
+		$loader.removeClass('alert').hide();
+		$spinner.hide();
+	};
 
-		var error = function error(message) {
-			$spinner.hide();
-			$loader.addClass('alert').show();
-			$error.text(message).show();	
-		};
+	var error = function error(message) {
+		$spinner.hide();
+		$loader.addClass('alert').show();
+		$error.text(message).show();	
+	};
 
-		return {
-			show: show,
-			hide: hide,
-			error: error
-		};
-	})();
+	MMCLapTimer.loader = {
+		show: show,
+		hide: hide,
+		error: error
+	};
 });
